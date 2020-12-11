5446ca13f744a5a5c93acd09f61478a7a8379e9a
Albert <wi18b088@technikum-wien.at>
2020-12-11 19:09:05 +0100

    Temperature, Humidity, change of Heating plot

--- plotting.py
+++ plotting.py
@@ -5,13 +5,28 @@ import matplotlib
 
 # Import data
 sensordata = pd.read_excel("data/sensor-data.xlsx", "data")
+sensordata.rename(columns={
+    'Set Temperature [°C]':'Temperature',
+    'Humidity [%]':'Humidity',
+    'Heater? [1=on, 0=nothing, -1=ventilator]':'Heater',
+}, inplace=True)
 
 # Show data
-print(sensordata)
+print(sensordata.columns.values)
 # print(sensordata.describe())
 # print(sensordata.count())
 
 # Plot data
 # matplotlib.use("TKagg")
-sb.displot(sensordata['Set Temperature [°C]'])
+# sb.displot(sensordata['Set Temperature [°C]'])
+
+figure = sb.lineplot(data = sensordata['Temperature'])
+figure = sb.lineplot(data = sensordata['Humidity'])
+# figure = sb.lineplot(data = sensordata['Heater'])
+# plt.yticks([0,20,40,60,80,100])
+temp = 0
+for i, val in enumerate(sensordata['Heater']):
+    if val != temp:
+        plt.axvline(i)
+        temp = val
 plt.show()
