ec699cb5006229556622192b482080ba326f3ad5
Albert <wi18b088@technikum-wien.at>
2020-12-11 19:27:19 +0100

    new data source

--- plotting.py
+++ plotting.py
@@ -4,11 +4,13 @@ from matplotlib import pyplot as plt
 import matplotlib
 
 # Import data
-sensordata = pd.read_excel("data/sensor-data.xlsx", "data")
+sensordata = pd.read_excel("data/humi.xlsx")
+# sensordata = pd.read_excel("data/sensor-data.xlsx", "data")
 sensordata.rename(columns={
     'Set Temperature [°C]':'Temperature',
     'Humidity [%]':'Humidity',
     'Heater? [1=on, 0=nothing, -1=ventilator]':'Heater',
+    'Air Quality [-]':'Air Quality',
 }, inplace=True)
 
 # Show data
@@ -20,13 +22,14 @@ print(sensordata.columns.values)
 # matplotlib.use("TKagg")
 # sb.displot(sensordata['Set Temperature [°C]'])
 
-figure = sb.lineplot(data = sensordata['Temperature'])
-figure = sb.lineplot(data = sensordata['Humidity'])
+figure = sb.lineplot(x = sensordata['Timestamp [s]'], y = sensordata['Temperature'])
+figure = sb.lineplot(x = sensordata['Timestamp [s]'], y = sensordata['Humidity']*100)
 # figure = sb.lineplot(data = sensordata['Heater'])
+figure = sb.lineplot(x = sensordata['Timestamp [s]'], y = sensordata['Air Quality'])
 # plt.yticks([0,20,40,60,80,100])
 temp = 0
 for i, val in enumerate(sensordata['Heater']):
     if val != temp:
-        plt.axvline(i)
+        plt.axvline(sensordata.iloc[i]['Timestamp [s]'])
         temp = val
 plt.show()
