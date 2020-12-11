f23a3893125825f8c90fc41a20aaf69c68df28f5
Albert <wi18b088@technikum-wien.at>
2020-12-11 23:34:51 +0100

    Working temperature example

--- mqtt-subscriber.py
+++ mqtt-subscriber.py
@@ -1,8 +1,16 @@
 import paho.mqtt.client as mqtt
 import time
 
+temperature = 20
 def on_message(client, userdata, message):
+    global temperature
     print(f"received message: {str(message.payload.decode('utf8'))}")
+    if str(message.payload.decode('utf8')) == "UP":
+        temperature += 1
+    if str(message.payload.decode('utf8')) == "DOWN":
+        temperature -= 1
+    print(f"New temperature: {temperature}")
+    
 
 mqttBroker = "broker.hivemq.com"
 
@@ -10,9 +18,10 @@ client = mqtt.Client("Smartphone")
 client.connect(mqttBroker)
 
 client.loop_start()
+print(f"Temperature is {temperature}")
 print("Waiting for messages...")
 
-client.subscribe("TEMPERATURE")
+client.subscribe("TEMPCONTROL")
 client.on_message=on_message
 
 time.sleep(30)
