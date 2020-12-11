8943ef4eafb27f88967fe32c0730a9bc7d008b6b
Albert <wi18b088@technikum-wien.at>
2020-12-11 18:31:07 +0100

    first visualisation with seaborn and tk

--- plotting.py
+++ plotting.py
@@ -1,1 +1,17 @@
-print("Hello Python")
+import pandas as pd
+import seaborn as sb
+from matplotlib import pyplot as plt
+import matplotlib
+
+# Import data
+sensordata = pd.read_excel("/home/albert/Desktop/Pietro/PythonPlotting/data/sensor-data.xlsx", "data")
+
+# Show data
+print(sensordata)
+# print(sensordata.describe())
+# print(sensordata.count())
+
+# Plot data
+# matplotlib.use("TKagg")
+sb.displot(sensordata['Set Temperature [°C]'])
+plt.show()
